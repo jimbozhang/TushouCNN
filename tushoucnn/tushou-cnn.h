2e91bd480d67cb4a1c7d7dd4698b28f3240c53ae
Junbo Zhang <dr.jimbozhang@gmail.com>
2018-01-04 05:54:27 +0800

    Load model in plain text format.

--- tushoucnn/tushou-cnn.h
+++ tushoucnn/tushou-cnn.h
@@ -16,23 +16,69 @@
 #ifndef TUSHOUCNN_TUSHOU_CNN_H
 #define TUSHOUCNN_TUSHOU_CNN_H
 
+#include <fstream>
+#include <vector>
 #include <string>
+#include <map>
+#include <cassert>
 
 namespace tushoucnn {
 
 typedef float FeatType;
 typedef int LabelType;
 
+struct Layer {
+  std::string type;
+  std::map<std::string, std::string> hparams;
+};
+
 class NN {
 public:
   NN() {}
 
-  void load_model(std::string model_dir) {
+  void load_model(std::string model_dir, bool binary = false) {
+    std::string model_file = model_dir + "/model";
+    if (binary) {
+      std::cerr << "Unsupported format." << std::endl;
+      return;
+    }
+    else {
+      std::ifstream fin;
+      fin.open(model_file.c_str());
+      Layer *cur_layer = NULL;
+      while (! fin.eof()) {
+        std::string tok_key, tok_val;
+        fin >> tok_key;
+        fin >> tok_val;
+        if (tok_key == "layer") {
+          // new layer
+          if (cur_layer != NULL) {
+            layers.push_back(*cur_layer);
+            delete cur_layer;
+          }
+          cur_layer = new Layer;
+          cur_layer->type = tok_val;
+        }
+        else {
+          // this layer
+          assert(cur_layer != NULL);
+          cur_layer->hparams[tok_key] = tok_val;
+        }
+     }
+     if (cur_layer != NULL) {
+       layers.push_back(*cur_layer);
+       delete cur_layer;
+     }
+     fin.close();
+    }
   }
 
   LabelType predict(std::vector<FeatType> feats) {
     return 7;  // lucky number
   }
+
+private:
+  std::vector<Layer> layers;
 
 };
 
